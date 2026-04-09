5a8febab87042fbeb34f22395b56630e0e2eacf4
Mohanraj T-S <mohanrajts400@gmail.com>
2026-04-09 11:46:44 +0000

    Delete printone directory

--- printone/printone/printone/doctype/printer_contract/printer_contract.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2026, Mohanraj and contributors
-# For license information, please see license.txt
-
-# import frappe
-from frappe.model.document import Document
-
-
-class PrinterContract(Document):
-	pass
